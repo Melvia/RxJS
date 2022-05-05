e5b3c98bf18e9fc72112a7abebd1da27adc206a0
Руденко Наталья Сергеевна <rudenko@technocom.tech>
2022-05-05 10:03:49 +0500

    first rxjs

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
+/*
 const sequence = new Promise((res)=> {let count=1;
 setInterval(()=>{res(count++);})})
 
 
 sequence.then((v)=>console.log(v));
 sequence.then((v)=>console.log(v));
+*/
+
+import { interval} from "rxjs";
+
+interval(1000).subscribe((v:number)=>{
+    console.log(v)
+})
