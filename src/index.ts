49194538017f1bcc26d3e07d2c62ced09213dc16
Руденко Наталья Сергеевна <rudenko@technocom.tech>
2022-05-06 17:27:50 +0500

    subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-import './swipe'
+import { interval, Observable, Subscriber } from "rxjs";
+
+
+function doNothing(source$: Observable<any>) {
+    return source$
+}
+
+//на самом деле мы подписались на этот поток
+function toText(source$:Observable<any>) {
+    return new Observable(subscriber => {
+        subscriber.next('RxJS is Awesome!!!')
+        subscriber.complete();
+    })
+}
+
+class DoubleSubscriber extends Subscriber<number>{
+    next(value:number):void {
+        super.next(value * 2);
+    }
+}
+
+function double(source$:Observable<any>) {
+    const o$ = new Observable();
+    o$.source = source$;
+    o$.operator = {
+        call(subscriber:Subscriber<unknown>, source: any): void {
+            source.subscribe(new DoubleSubscriber(subscriber))
+        }
+    }
+    return o$;
+}
+
+interval(1000)
+.pipe(
+    double
+)
+.subscribe((v)=>{
+    console.log(v)
+}, ()=>{}, ()=>{console.log('complete')})
